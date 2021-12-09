5157651fed4a6098eca45eb1988192c8f9dd28f0
root <root@localhost.localdomain>
2021-12-09 04:54:04 +0100

    add tomcat9.service

--- tomcat_deploy.sh
+++ tomcat_deploy.sh
@@ -11,25 +11,31 @@ echo ""
 echo ""
 echo "launch container"
 echo "---------------------------------"
-docker run --name myapp_container --rm -dt --privileged=true -v ${PWD}:/data myapp bash
+docker run --name myapp_container --rm -it --privileged=true -v ${PWD}:/data myapp bash
 echo ""
 
 echo ""
 echo "update repo in docker container"
 echo "---------------------------------"
-docker exec -it myapp_container apt-get -y update
+#docker exec -it myapp_container apt-get -y update
 echo ""
 
 echo ""
 echo "install curl ansible in docker container"
 echo "---------------------------------"
-docker exec -it myapp_container apt-get install --fix-missing -y curl ansible
+#docker exec -it myapp_container apt-get install --fix-missing -y curl ansible
 echo ""
 
 echo ""
-echo "edit hosts file in docker container"
+echo "edit ansible hosts file in docker container"
 echo "---------------------------------"
-docker exec -it myapp_container echo '[server]\nhost1 ansible_ssh_host=192.168.1.18\n' > /etc/ansible/hosts 
+#docker exec -it myapp_container ip addr 
+#docker exec -it myapp_container cat /etc/hosts 
+#docker exec -it myapp_container cat /etc/ansible/hosts 
+docker exec -it myapp_container echo -e '[local]\nlocalhost ansible_connection=local\n' > /etc/ansible/hosts
+docker exec -it myapp_container echo "hllo+++++++++++++++++++++++++" 
+docker exec -it myapp_container cat /etc/ansible/hosts 
+docker exec -it myapp_container echo "hllo+++++++++++++++++++++++++"
 echo ""
 
 echo ""
